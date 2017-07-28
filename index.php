e006bd78c903bf0c653bfc4dfb36ac1fd583d586
Thomas <thomas@localhost.localdomain>
2017-07-28 16:15:00 -0400

    update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<!DOCTYPE HTML>
+<?php
+?>
 <html>
-
+<!DOCTYPE HTML>
 <head>
     <title>Thomas R. Cameron</title>
 </head>
@@ -12,10 +13,10 @@
 <div class="navigation-bar">
     <ul>
         <li><a href="pages/cv.pdf" target="_blank">Curriculum Vitae</a></li>
-        <li><a href="pages/research.html">Research</a></li>
+        <li><a href="pages/research.php">Research</a></li>
         <li><a href="index.php"><img class="logo" src="pictures/logo.png"></a></li>
-        <li><a href="pages/courses.html">Courses</a></li>
-        <li><a href="pages/get_involved.html">Get Involved</a></li>
+        <li><a href="pages/courses.php">Courses</a></li>
+        <li><a href="pages/get_involved.php">Get Involved</a></li>
     </ul>
 </div>
 <hr class="hrstyle-one" style="width: 50%">
---
+++ pages/research.php
@@ -0,0 +1,62 @@
+<?php
+?>
+<html>
+<!DOCTYPE HTML>
+<head>
+    <title>Research</title>
+</head>
+
+<link rel="stylesheet" type="text/css" href="../stylesheet.css" media="screen" />
+
+<body>
+<!-- Navigation Bar -->
+<div class="navigation-bar">
+    <ul>
+        <li><a href="../pages/cv.pdf" target="_blank">Curriculum Vitae</a></li>
+        <li><a href="../pages/research.php">Research</a></li>
+        <li><a href="../index.php"><img class="logo" src="../pictures/logo.png"></a></li>
+        <li><a href="../pages/courses.php">Courses</a></li>
+        <li><a href="../pages/get_involved.php">Get Involved</a></li>
+    </ul>
+</div>
+<hr class="hrstyle-one" style="width: 50%">
+<!-- Split page into three parts -->
+<div style="width: 100%">
+  <!-- Publications -->
+  <div style="float: left; width: 50%">
+    <div class="header">
+      <h3>Publications</h3>
+    </div>
+    <hr class="hrstyle-one" style="width: 50%">
+    <div class="class-list">
+    <ul>
+        <li>Thomas R. Cameron and Panayiotis J. Psarrakos, <em>A generalization of Descartes' rule of signs for matrix polynomials</em>, In Progress (2017)</li>
+        <li>Thomas R. Cameron, <em>Determinants: from Signed Volume from Laplace's Formula</em>, In Review (2017)</li>
+        <li>Thomas R. Cameron, Nikolas I. Steckley, <em>On the application of Laguerre's method to the polynomial eigenvalue problem</em>, In Review (2017)</li>
+        <ul>
+            <li><a href="https://arxiv.org/abs/1703.08767" target="_blank">arXiv pre-print</a> | <a href="https://github.com/Nick314159/LMPEP" target="_blank">GitHub Repo</a></li>
+        </ul>
+        <li>Thomas R. Cameron, <em>On the reduction of matrix polynomials to Hessenberg form</em>, Electronic Journal of Linear Algebra, 31 (2016), 321-334.</li>
+        <ul>
+            <li><a href="http://repository.uwyo.edu/ela/vol31/iss1/24/" target="_blank">DOWNLOAD</a></li>
+        </ul>
+        <li>Thomas R. Cameron, <em>Spectral bounds for matrix polynomials with unitary coefficients</em>, Electronic Journal of Linear Algebra, 30 (2015), 585-591.</li>
+        <ul>
+            <li><a href="http://repository.uwyo.edu/ela/vol30/iss1/38/" target="_blank">DOWNLOAD</a></li>
+        </ul>
+    </ul>
+    </div>
+    <hr class="hrstyle-one" style="width: 50%">
+  </div>
+  <!-- Student Work -->
+  <div style="float: left; width: 50%">
+    <div class="header">
+      <h3>Student Work</h3>
+    </div>
+    <hr class="hrstyle-one" style="width: 50%">
+    <hr class="hrstyle-one" style="width: 50%">
+  </div>
+</div>
+</body>
+
+</html>

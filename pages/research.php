4e72273cc73f60352b423e99c6ad6a89e51d3e1c
Thomas <thomas@localhost.localdomain>
2017-07-29 11:03:53 -0400

    updated research page

--- pages/research.php
+++ pages/research.php
@@ -28,10 +28,10 @@
       <h3>Publications</h3>
     </div>
     <hr class="hrstyle-one" style="width: 50%">
-    <div class="class-list">
+    <div class="list">
     <ul>
         <li>Thomas R. Cameron and Panayiotis J. Psarrakos, <em>A generalization of Descartes' rule of signs for matrix polynomials</em>, In Progress (2017)</li>
-        <li>Thomas R. Cameron, <em>The Determinant: from Signed Volume from Laplace's Formula</em>, In Review (2017)</li>
+        <li>Thomas R. Cameron, <em>The Determinant: from Signed Volume to Laplace's Formula</em>, In Review (2017)</li>
         <li>Thomas R. Cameron, Nikolas I. Steckley, <em>On the application of Laguerre's method to the polynomial eigenvalue problem</em>, In Review (2017)
             <ul><li><a href="https://arxiv.org/abs/1703.08767" target="_blank">arXiv pre-print</a> | <a href="https://github.com/Nick314159/LMPEP" target="_blank">GitHub Repo</a></li></ul>
         </li>
@@ -51,6 +51,15 @@
       <h3>Student Work</h3>
     </div>
     <hr class="hrstyle-one" style="width: 50%">
+    <div class="list">
+    <ul>
+        <li>Leo Trujilo, <em>The numerical range of a matrix polynomial</em>, presented at the 2016-2017 College of Idaho undergraduate research fair, The College of Idaho.</li>
+        <li>Will Callahan, Sam Chandler, Johanna Mori, and Leo Trujilo, <em>Using Chebyshev polynomials to solve ordinary differential equations</em>, presented at the 2016 Murdock Undergraduate Conference, The College of Idaho.</li>
+        <li>Nikolas Steckley, <em>A personalized grade management system using MySQL and PHP</em>, Washington State University.</li>
+        <li>Grant Hutchings, <em>Numerical Algorithms for matrix computations and applications</em>, Washington State University.</li>
+        <li>Michael Newsham, <em>Bernstein Polynomials and companion matrices</em>, Washington State University.</li>
+    </ul>
+    </div>
     <hr class="hrstyle-one" style="width: 50%">
   </div>
 </div>

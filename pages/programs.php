9609c2bb8d8b3b46d768a73f3b9f2e0895a9425e
Thomas <thomas@localhost.localdomain>
2017-09-02 12:35:13 -0400

    updated research and programming pages

--- pages/programs.php
+++ pages/programs.php
@@ -3,7 +3,7 @@
 <html>
 <!DOCTYPE HTML>
 <head>
-    <title>Programming</title>
+    <title>Programs</title>
 </head>
 
 <link rel="stylesheet" type="text/css" href="../stylesheet.css" media="screen" />
@@ -20,6 +20,21 @@
     </ul>
 </div>
 <hr class="hrstyle-one" style="width: 50%">
+<div style="width: 100%">
+    <div class="list">
+    <ul>
+        <li>Thomas R. Cameron, <em>The Testing of Descartes' Rule of Signs for Matrix Polynomials</em>, 2017
+            <ul><li><a href="program_files/drsmp/DRSMP_testing.py">Download Python File</a></li></ul>
+        </li>
+        <li>Thomas R. Cameron, <em>The Numerical Range of a Matrix Polynomial and its Derivative</em>, 2017
+            <ul><li><a href="program_files/drsmp/num_range_der.py">Download Python File</a></li></ul>
+        </li>
+        <li>Thomas R. Cameron, Leonardo Trujillo, <em>The Numerical Range of a Matrix Polynomial</em>, 2017
+            <ul><li><a href="program_files/drsmp/num_range.py">Download Python File</a></li></ul>
+        </li>
+    </ul>
+    </div>
+</div>
 </body>
 
 </html>
--- pages/research.php
+++ pages/research.php
@@ -33,7 +33,7 @@
         <li>Thomas R. Cameron and Panayiotis J. Psarrakos, <em>A generalization of Descartes' rule of signs for matrix polynomials</em>, In Progress (2017)</li>
         <li>Thomas R. Cameron, <em>The Determinant: from Signed Volume to Laplace's Formula</em>, In Review (2017)</li>
         <li>Thomas R. Cameron, Nikolas I. Steckley, <em>On the application of Laguerre's method to the polynomial eigenvalue problem</em>, In Review (2017)
-            <ul><li><a href="https://arxiv.org/abs/1703.08767" target="_blank">arXiv pre-print</a> | <a href="https://github.com/Nick314159/LMPEP" target="_blank">GitHub Repo</a></li></ul>
+            <ul><li><a href="https://arxiv.org/abs/1703.08767" target="_blank">arXiv pre-print</a></li></ul>
         </li>
         <li>Thomas R. Cameron, <em>On the reduction of matrix polynomials to Hessenberg form</em>, Electronic Journal of Linear Algebra, 31 (2016), 321-334.
             <ul><li><a href="http://repository.uwyo.edu/ela/vol31/iss1/24/" target="_blank">DOWNLOAD</a></li></ul>

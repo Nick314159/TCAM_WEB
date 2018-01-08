5124ecd74ded66facec32a5f95d6b555241b44b4
Cameron <thcameron@w15166.davidson.edu>
2018-01-08 14:06:18 -0500

    update to mat112 schedule and syllabus

---
+++ courses/MAT-112/mat_112.php
@@ -0,0 +1,296 @@
+<?php
+?>
+<html>
+<!DOCTYPE HTML>
+<head>
+    <title>Calculus I and Modeling</title>
+</head>
+
+<link rel='stylesheet' type='text/css' href='../../stylesheet.css'>
+
+<body>
+<!-- Title -->
+<div class="title">
+  <h2>MAT-112: Calculus I and Modeling</h2>
+</div>
+<!-- Split page into two parts -->
+<div style="width: 100%">
+  <!--Left Margin -->
+  <div style="float: left; width: 30%">
+  <!-- Info -->
+  <div class="header">
+    <h3>Course Information</h3>
+  </div>
+  <hr class="hrstyle-one" style="width: 50%">
+  <div class="list">
+    <ul>
+      <li><strong>Time: </strong> M,W,F 2:30 - 3:20 pm</li>
+      <li><strong>Place: </strong> CHAM 2187</li>
+      <li><strong>Important Dates</strong>
+      <hr class="hrstyle-two" style="width: 25%">
+        <ul>
+          <li><strong>Review 1: </strong>February</li>
+          <li><strong>Review 2: </strong>March</li>
+          <li><strong>Review 3: </strong>April</li>
+		  <li><strong>Final Review: </strong>May</li>
+        </ul>
+      </li>
+      <li><strong>Links </strong>
+      <hr class="hrstyle-two" style="width: 25%">
+        <ul>
+          <li><a href="syllabus/mat_112_spring2018_syllabus.pdf" target="_blank">Syllabus</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <hr class="hrstyle-one" style="width: 50%">
+  </div>
+  <!-- Right Margin -->
+  <div style="float: left; width: 70%">
+  <!-- Calender -->
+  <div class="header">
+    <h3>Calender of Events</h3>
+  </div>
+  <hr class="hrstyle-one" style="width: 50%">
+  <div class="schedule-container">
+    <!-- Week 1 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 1 (1/15 - 1/19)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td>MLK Day</td><td>Lines and Functions.<br><strong>Read Sections 1.1 and 1.3</strong></td><td>Quadratic, Polynomial<br>and Rational Functions.<br><strong>Read Sections 1.4 - 1.5</strong></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 2 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 2 (1/22 - 1/26)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td>Exponential and Log Functions.<br><strong>Read Sections 2.1 - 2.2</strong></td><td>Applications.<br><strong>Read Section 2.3</strong></td><td>Trig Functions.<br><strong>Read Section 2.4</strong><br><strong><a href="" target="_blank">Homework 1: Due 1/26</a></strong></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 3 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 3 (1/29 - 2/2)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 4 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 4 (2/5 - 2/9)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 5 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 5 (2/12 - 2/16)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 6 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 6 (2/19 - 2/23)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 7 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 7 (2/26 - 3/2)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 8 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 8 (3/5 - 3/9)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td>Spring Break</td><td>Spring Break</td><td>Spring Break</td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 9 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 9 (3/12 - 3/16)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 10 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 10 (3/19 - 3/23)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 11 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 12 (3/26 - 3/30)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 12 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 12 (4/2 - 4/6)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td>Easter Break</td><td>Easter Break</td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 13 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 13 (4/9 - 4/13)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 14 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 14 (4/16 - 4/20)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 15 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 15 (4/23 - 4/27)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 16 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 16 (4/30 - 5/4)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td></td><td></td>
+      </tr>
+      </table>
+    </div>
+    <!-- Week 17 -->
+    <div class="mySlides fade">
+      <div class="header"><h3>Week 17 (5/7 - 5/11)</h3></div>
+      <table width="100%" align="center" class="schedule">
+      <tr>
+        <th>Monday</th><th>Wednesday</th><th>Friday</th>
+      </tr>
+      <tr>
+        <td></td><td>Have a Great Summer</td><td>Have a Great Summer</td>
+      </tr>
+      </table>
+    </div>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
+  <script>
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  }
+  </script>
+  <hr class="hrstyle-one" style="width: 50%">
+  </div>
+</div>
+</body>
+
+</html>

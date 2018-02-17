8c8f93458c3f1e28cd652eadd23edf448c823627
Cameron <thcameron@w15166.davidson.edu>
2018-02-17 13:01:27 -0500

    updated cv and research page

--- research/research.php
+++ research/research.php
@@ -30,17 +30,12 @@
     <hr class="hrstyle-one" style="width: 50%">
     <div class="list">
     <ul>
-        <li>Thomas R. Cameron and Panayiotis J. Psarrakos, <em>A generalization of Descartes' rule of signs for matrix polynomials</em>, In Progress (2017)</li>
-        <li>Thomas R. Cameron, Nikolas I. Steckley, <em>On the application of Laguerre's method to the polynomial eigenvalue problem</em>, In Progress (2017)
-            <ul><li><a href="https://arxiv.org/abs/1703.08767" target="_blank">arXiv pre-print</a></li></ul>
-        </li>
-        <li>Thomas R. Cameron, <em>The Determinant: from Signed Volume to Laplace's Formula</em>, In Review (2017)</li>
-        <li>Thomas R. Cameron, <em>On the reduction of matrix polynomials to Hessenberg form</em>, Electronic Journal of Linear Algebra, 31 (2016), 321-334.
-            <ul><li><a href="http://repository.uwyo.edu/ela/vol31/iss1/24/" target="_blank">DOWNLOAD</a></li></ul>
-        </li>
-        <li>Thomas R. Cameron, <em>Spectral bounds for matrix polynomials with unitary coefficients</em>, Electronic Journal of Linear Algebra, 30 (2015), 585-591.
-            <ul><li><a href="http://repository.uwyo.edu/ela/vol30/iss1/38/" target="_blank">DOWNLOAD</a></li></ul>
-        </li>
+		<li>Cameron, T. R. (2018). A practical parallelizable fourth order modification of Laguerre's method. <em>In Progress</em>.</li>
+        <li>Cameron, T. R., Psarrakos, P. J. (2018). On Descartes' rule of signs for matrix polynomials. <em>Submitted</em>.</li>
+		<li>Cameron, T. R. (2018) The determinant from signed volume to the Laplace expansion. <em>To Appear in Amer. Math. Monthly</em>, accepted on 2/12/2018.</li>
+        <li>Cameron, T. R., Steckley, N. I. (2017). On the application of Laguerre's method to the polynomial eigenvalue problem. <em>Working Paper</em>. <a href="https://arxiv.org/abs/1703.08767">arXiv:1703.08767 [math.NA]</a></li>
+        <li>Cameron, T. R. (2016). On the reduction of matrix polynomials to Hessenberg form. <em>ELA</em>, 31: 321-334. <a href="https://doi.org/10.13001/1081-3810.3011" target="_blank">doi.org/10.13001/1081-3810.3011</a></li>
+        <li>Cameron, T. R. (2015). Spectral bounds for matrix polynomials with unitary coefficients. <em>ELA</em>, 30: 585-591. <a href="https://doi.org/10.13001/1081-3810.2911" target="_blank">doi.org/10.13001/1081-3810.2911</a></li>
     </ul>
     </div>
     <hr class="hrstyle-one" style="width: 50%">
@@ -53,6 +48,8 @@
     <hr class="hrstyle-one" style="width: 50%">
     <div class="list">
     <ul>
+		<li>Huseyin Altinisik, Max Li, Pasha Zonkin, and Jenny Zhong. <em>Heterogeneous Computing and OpenCL</em>, Ongoing.</li>
+		<li>Nick Steckley, <em>On modifications to Laguerre's method and the polynomial eigenvalue problem</em>, presented at the 2017 PNWNAS.</li>
         <li>Leo Trujilo, <em>The numerical range of a matrix polynomial</em>, presented at the 2016-2017 College of Idaho Undergraduate Research Conference, The College of Idaho.</li>
         <li>Will Callahan, Sam Chandler, Johanna Mori, and Leo Trujilo, <em>Using Chebyshev polynomials to solve ordinary differential equations</em>, presented at the 2016 Murdock Undergraduate Research Conference, The College of Idaho.</li>
         <li>Nikolas Steckley, <em>A personalized grade management system using MySQL and PHP</em>, Washington State University.</li>
